6f96da9a03e050dda1b02dd19c76a2da0f0b1549
SeokhyeonHong <37925049+SeokhyeonHong@users.noreply.github.com>
2021-01-25 14:56:45 +0900

    first commit
    added foot IK files

---
+++ Source/FootIK/FootIKGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FootIKGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFootIKGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFootIKGameMode();
+};
+
+
+
